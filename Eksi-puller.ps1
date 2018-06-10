be4decc650d998d688f7f82cf7bea07f845f1411
Enis Kollugil <kollugil@itu.edu.tr>
2018-06-10 13:03:46 +0300

    Pull All Titles

--- Eksi-puller.ps1
+++ Eksi-puller.ps1
@@ -1,3 +1,13 @@
 #Windonis
-$i = 1
-(((Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/$i).Links).href) | Select-String "day"
+#EksiSozluk Puller
+
+$data = Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/2 
+$pageCount = ($data.AllElements | Where-Object class -eq "pager")."data-pagecount" | get-unique
+
+function Get-allTitles () 
+{
+    for ($i = 1; $i -le $pageCount; $i++) 
+    {
+        (((Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/$i).Links).href) | Select-String "day" >> D:\\enissil.txt
+    }    
+}
