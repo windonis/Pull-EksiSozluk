647223a16c00de0b3b4da97e074ce2a252477cd5
windonis <40137486+windonis@users.noreply.github.com>
2018-06-10 15:49:38 +0300

    Searching

--- Eksi-puller.ps1
+++ Eksi-puller.ps1
@@ -1,32 +1,47 @@
 #Windonis
 #EksiSozluk Puller
 
-$data = Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/2 
+$data = Invoke-WebRequest "https://eksisozluk.com/basliklar/bugun/2"
 $pageCount = ($data.AllElements | Where-Object class -eq "pager")."data-pagecount" | get-unique
-$count = 5;
-function get-titles ($element) 
+#$pageCount = 4
+$allTitles = New-Object System.Collections.ArrayList($null)
+$tmp = New-TemporaryFile
+function Get-titles ($element, $keyWord) 
 {
-    $allTitles = New-Object System.Collections.ArrayList($null)
     $allTitles += $element
-    #Start-Sleep 30
-    Set-Clean -link $allTitles
+    Set-Clean -link $allTitles -keyWord $keyWord
 }
-function Get-allTitles () 
+function Get-allTitles ($keyWord) 
 {
     for ($i = 1; $i -le $pageCount; $i++) 
     {
         $titles = (((Invoke-WebRequest "https://eksisozluk.com/basliklar/bugun/$i").Links).href) | Select-String "day" 
         foreach ($title in $titles) 
         {
-            get-Titles -element "$title"
-        }    
+            get-Titles -element "$title" -keyWord $keyWord
+        }
+        
     }   
 }
-function Set-Clean ($link) 
+function Set-Clean ($link, $keyWord) 
 {
-
     $cleanLink = $link |ForEach-Object{$_.split("?")[0]}
-    return $cleanLink
+    if ($cleanLink -like "*$keyword*")
+    {
+        $cleanLink >> $tmp.FullName
+    }
+    
 }
 
-Get-AllTitles
+function Get-TitleForKeyword ($keyWord) 
+{
+    $titles = Get-Content $tmp.Fullname
+    foreach ($title in $titles) {
+        if ($title -like "*$keyWord*") 
+        {
+            Write-Host $title
+        }
+    }    
+}
+
+Get-AllTitles -keyWord "muharrem"
