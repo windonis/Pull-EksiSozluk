dcee6e5eaecc97423dc8f53fe3c77f8484fdf21b
windonis <40137486+windonis@users.noreply.github.com>
2018-06-10 13:25:57 +0300

    titles to array

--- Eksi-puller.ps1
+++ Eksi-puller.ps1
@@ -8,6 +8,16 @@ function Get-allTitles ()
 {
     for ($i = 1; $i -le $pageCount; $i++) 
     {
-        (((Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/$i).Links).href) | Select-String "day" >> D:\\enissil.txt
+        $tempArray = New-Object System.Collections.ArrayList($null)
+        $titles = (((Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/$i).Links).href) | Select-String "day"
+        $tempArray.AddRange($titles)
     }    
 }
+
+function Set-Clean ($link) 
+{
+    $cleanLink = $link.split("?")[0]
+    return $cleanLink
+}
+
+
