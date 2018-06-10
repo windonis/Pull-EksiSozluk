41b5ddb45db232f147d66349e4465e388cadb7ab
windonis <40137486+windonis@users.noreply.github.com>
2018-06-10 14:24:46 +0300

    AllTitles Pushed

--- Eksi-puller.ps1
+++ Eksi-puller.ps1
@@ -3,21 +3,30 @@
 
 $data = Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/2 
 $pageCount = ($data.AllElements | Where-Object class -eq "pager")."data-pagecount" | get-unique
-
+$count = 5;
+function get-titles ($element) 
+{
+    $allTitles = New-Object System.Collections.ArrayList($null)
+    $allTitles += $element
+    #Start-Sleep 30
+    Set-Clean -link $allTitles
+}
 function Get-allTitles () 
 {
     for ($i = 1; $i -le $pageCount; $i++) 
     {
-        $tempArray = New-Object System.Collections.ArrayList($null)
-        $titles = (((Invoke-WebRequest https://eksisozluk.com/basliklar/bugun/$i).Links).href) | Select-String "day"
-        $tempArray.AddRange($titles)
-    }    
+        $titles = (((Invoke-WebRequest "https://eksisozluk.com/basliklar/bugun/$i").Links).href) | Select-String "day" 
+        foreach ($title in $titles) 
+        {
+            get-Titles -element "$title"
+        }    
+    }   
 }
-
 function Set-Clean ($link) 
 {
-    $cleanLink = $link.split("?")[0]
+
+    $cleanLink = $link |ForEach-Object{$_.split("?")[0]}
     return $cleanLink
 }
 
-
+Get-AllTitles
